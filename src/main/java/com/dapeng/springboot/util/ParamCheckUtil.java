2625e7f57edca5a0ff1876bdf298cb63c9f538aa
lipeng <281828254@qq.com>
2020-03-08 21:34:40 +0800

    暂时只有数字和字母控制

--- src/main/java/com/dapeng/springboot/util/ParamCheckUtil.java
+++ src/main/java/com/dapeng/springboot/util/ParamCheckUtil.java
@@ -11,8 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ParamCheckUtil {
 
-    //邮箱必须是数字加英文，可以包含特殊字符
-    private final static String reqFormat = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z~@*/#()_]{8,16}$";
+    //邮箱必须是数字加英文，可以包含特殊字符^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z~@*/#()_]{8,16}$"
+    private final static String reqFormat = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$";
     /**
      * 密码组合验证
      * @param password
@@ -22,11 +22,11 @@ public class ParamCheckUtil {
     }
 
     public static void main(String[] args) {
-        String loginName = "1a311aa131";
+        String loginName = "1a311a#a131";
         String t1 = "^[0-9A-Za-z]{2,16}$"; //判断密码用户名和密码是否为数字，字母
         String t2="^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z~@*/#()_]{8,16}$";
-        if (loginName.matches(t2)){
-            System.out.println(loginName.matches(t2));
+        if (loginName.matches(reqFormat)){
+            System.out.println(loginName.matches(reqFormat));
         } else {
             System.out.println("在这里");
         }
