b0b0aa65ab1268ab06ad8d66a02184e023e18adf
lipeng <281828254@qq.com>
2020-03-10 17:38:02 +0800

    团队实体

--- src/main/java/com/dapeng/springboot/entity/TeamEntity.java
+++ src/main/java/com/dapeng/springboot/entity/TeamEntity.java
@@ -10,34 +10,25 @@ import java.util.Date;
 /**
  * @author lipeng
  * @version 1.0
- * @date 2020/3/7 23:32
- * @message：用户信息entity
+ * @date 2020/3/10 17:09
+ * @message：
  */
 @Setter
 @Getter
 @Entity
-@Table(name = "user_info")
-public class UserInfoEntity implements Serializable {
+@Table(name = "team_info")
+public class TeamEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//自增设置
     @Column(name = "id",columnDefinition = "bigint(18) comment '主键'")
     private Long id;
 
-    @Column(name = "user_name",columnDefinition = "varchar(64) comment '姓名'")
-    private String userName;
+    @Column(name = "create_by",columnDefinition = "bigint(18) comment '创建人'")
+    private Long createBy;
 
-    @Column(name = "login_name",columnDefinition = "varchar(64) comment '登录名'")
-    private String loginName;
-
-    @Column(name = "password",columnDefinition = "varchar(256) comment '密码'")
-    private String password;
-
-    @Column(name = "email")
-    private String email;
-
-    @Column(name = "head_img_id")
-    private String headImgId;
+    @Column(name = "create_name",columnDefinition = "varchar(64) comment '创建人名称'")
+    private String createName;
 
     @Column(
             name = "create_time",
@@ -54,16 +45,4 @@ public class UserInfoEntity implements Serializable {
             columnDefinition = "timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'"
     )
     private Date updateTime;
-
-    //成员，团长
-    @Column(name = "user_type")
-    private String userType;
-
-    @Column(name = "term_id")
-    private Long termId;
-
-    @Column(name = "position")
-    private String position;
-
-
 }
